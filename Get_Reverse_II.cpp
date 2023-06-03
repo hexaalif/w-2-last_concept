9d6941c2853332960fd573bf5e6fd326984e690e
hexaalif <hexaalif2020@gmail.com>
2023-06-03 17:42:54 +0600

    file

--- Get_Reverse_II.cpp
+++ Get_Reverse_II.cpp
@@ -2,8 +2,49 @@
 
 using namespace std;
 
+class Rev2
+{
+public:
+    string nm;
+    int cls;
+    string s;
+    int id;
+};
+
 int main()
 {
+    int n;
+    cin >> n;
+    Rev2 obj[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> obj[i].nm;
+        cin >> obj[i].cls;
+        cin >> obj[i].s;
+        cin >> obj[i].id;
+    }
+    // Rev2 mx;
+    // mx.id = INT_MAX;
+    // sort(obj, obj+n, swap);
 
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (obj[i].id < obj[j].id)
+            {
+                swap(obj[i].id, obj[j].id);
+            }
+            else
+            {
+                swap(obj[j].id, obj[i].id);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << obj[i].nm << " " << obj[i].cls << " " << obj[i].s << " " << obj[i].id << endl;
+    }
     return 0;
 }
