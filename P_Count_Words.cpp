b69c2316b23baab6014cc1c1caa96066e612ab41
hexaalif <hexaalif2020@gmail.com>
2023-06-05 13:25:28 +0600

    count words solved

---
+++ P_Count_Words.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    bool insideWord = false;
+    stringstream ss(s);
+    string word;
+    int cnt = 0;
+    for (char c : s)
+    {
+        if (isalpha(c))
+        {
+            if (insideWord == false)
+            {
+                cnt++;
+            }
+            insideWord = true;
+        }
+        else
+        {
+            insideWord = false;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
